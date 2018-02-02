0af3ee87ff297bdde7cc361b27b9bda3528907c1
Nelson Touron <nelson.touron@gmail.com>
2018-02-02 18:14:30 +0100

    _transitionDuration from 750 to 0

--- src/base-mixin.js
+++ src/base-mixin.js
@@ -50,7 +50,7 @@ dc.baseMixin = function (_chart) {
     var _renderTitle = true;
     var _controlsUseVisibility = false;
 
-    var _transitionDuration = 50;
+    var _transitionDuration = 0;
 
     var _transitionDelay = 0;
 
